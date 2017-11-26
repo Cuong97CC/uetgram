f0c7e06fab6ff85717632cf8fc9114899a38ebdb
HangUet <hangtran9397@gmail.com>
2017-11-26 16:10:20 +0700

    Edit table and fake admin

--- database/migrations/2017_11_26_155615_create_images_users_table.php
+++ database/migrations/2017_11_26_155615_create_images_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateImagesTable extends Migration
+class CreateImagesUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateImagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('images', function (Blueprint $table) {
+        Schema::create('images_users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title')->nullable();
-            $table->string('content')->nullable();
-            $table->integer('idAlbum');
+            $table->integer('idImg');
             $table->integer('idUser');
-            $table->string('img');
-            $table->integer('mode')->default(0);
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateImagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('images');
+         Schema::dropIfExists('images_users');
     }
 }
